6a2bf3a01b4e0e312e51bc0cffa8a8de2e6dad9e
unknown <karen_tankaiyong@hotmail.com>
2018-02-14 12:44:47 +0800

    add new postal code class

---
+++ src/seedu/addressbook/data/person/PostalCode.java
@@ -0,0 +1,42 @@
+package seedu.addressbook.data.person;
+
+import javafx.geometry.Pos;
+
+public class PostalCode {
+    public static final String EXAMPLE = "634972";
+    public final int postalCode;
+
+    /**
+     * Construct postal code object with empty block value
+     */
+    public PostalCode(){
+        this.postalCode = 0;
+    }
+
+    /**
+     * Construct postal code object with value given.
+     *
+     */
+    public PostalCode(String postalCode){
+
+        this.postalCode = Integer.parseInt(postalCode);
+    }
+
+
+    @Override
+    public String toString() {
+        return String.valueOf(this.postalCode);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof PostalCode // instanceof handles nulls
+                && this.postalCode == ((PostalCode) other).postalCode );// state check
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(this.postalCode);
+    }
+}
