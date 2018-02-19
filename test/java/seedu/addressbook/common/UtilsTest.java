61a65035467d9afb071f42b05dbb95d9b1b4def5
unknown <karen_tankaiyong@hotmail.com>
2018-02-19 21:42:10 +0800

    update junit test by adding new tests on isAnyNull method in Utils class

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -1,6 +1,7 @@
 package seedu.addressbook.common;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
@@ -8,8 +9,31 @@ import java.util.List;
 
 import org.junit.Test;
 
+import javax.rmi.CORBA.Util;
+
 public class UtilsTest {
 
+    @Test
+    public void isAnyNull() throws Exception {
+        //only one not null object
+        assertIsNotNull(1);
+        assertIsNotNull("");
+        assertIsNotNull("abc");
+
+        //multiple not null objects
+        assertIsNotNull(123, "abc", "34gnsd");
+        assertIsNotNull("abded", 123456);
+
+        //multiple objects, some contain null
+        assertIsNull(null, 123, "adfje");
+        assertIsNull("sdga", null, 2134);
+        assertIsNull(1234, "mary", null);
+        assertIsNull(1235, null);
+
+        //all null objects
+        assertIsNull(null, null);
+        assertIsNull(null, null, null);
+    }
 
     @Test
     public void elementsAreUnique() throws Exception {
@@ -42,5 +66,13 @@ public class UtilsTest {
 
     private void assertNotUnique(Object... objects) {
         assertFalse(Utils.elementsAreUnique(Arrays.asList(objects)));
+    }
+
+    private void assertIsNull(Object... objects){
+        assertTrue(Utils.isAnyNull(objects));
+    }
+
+    private void assertIsNotNull(Object... objects){
+        assertFalse(Utils.isAnyNull(objects));
     }
 }
